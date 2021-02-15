e2e5863efc9e38123d9eaf02599e6e3131268679
Matheus Hudson de Souza <matheushudson2013@gmail.com>
2021-02-15 16:21:16 -0300

    Client created

---
+++ DevSuperioDesafio/src/test/java/com/devSuperiorDesafio/DevSuperioDesafio/DevSuperioDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devSuperiorDesafio.DevSuperioDesafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevSuperioDesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
